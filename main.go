fecdf2af993a61543032f3d8800d78d21f57c75e
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2018-05-23 22:29:06 -0700

    Clean up

--- main.go
+++ main.go
@@ -6,32 +6,10 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"regexp"
+	"strings"
+
+	"github.com/bradfitz/slice"
 )
-
-import "github.com/bradfitz/slice"
-
-type Deployments struct {
-	Items []struct {
-		Meta struct {
-			Name      string `json:"name"`
-			Namespace string `json:"namespace"`
-		} `json:"metadata"`
-	} `json:"items"`
-}
-
-type Deployments1 struct {
-	Items []Deployment `json:"items"`
-}
-
-type Deployment struct {
-	Meta Metadata `json:"metadata"`
-}
-
-type Metadata struct {
-	Name      string `json:"name"`
-	Namespace string `json:"namespace"`
-}
 
 func main() {
 	flag.Parse()
@@ -39,18 +17,7 @@ func main() {
 	search := flag.Arg(0)
 	cmd := flag.Arg(1)
 	context := os.Getenv("KUBECTL_PLUGINS_GLOBAL_FLAG_CONTEXT")
-
-	// fmt.Println("FLAG!!!", search)
-	// env := os.Environ()
-
-	// for k, v := range env {
-	// 	fmt.Printf("%s: %s\n", k, v)
-	// }
-
 	kubectl := os.Getenv("KUBECTL_PLUGINS_CALLER")
-	// fmt.Println(kubectl)
-
-	searchRegexp := regexp.MustCompile(fmt.Sprintf(".*%s.*", search))
 
 	type PodMeta struct {
 		Name      string `json:"name"`
@@ -65,11 +32,10 @@ func main() {
 		Items []Pod `json:"items"`
 	}
 
-	podsJSON, err2 := exec.Command(kubectl, "--context", context, "get", "--raw=/api/v1/pods").Output()
+	podsJSON, getError := exec.Command(kubectl, "--context", context, "get", "--raw=/api/v1/pods").Output()
 
-	// fmt.Printf("PODS %s", podsJSON)
-	if err2 != nil {
-		panic(err2)
+	if getError != nil {
+		panic(getError)
 	}
 
 	var pods Pods
@@ -85,9 +51,7 @@ func main() {
 	var matchingPods []Pod
 
 	for _, pod := range pods.Items {
-		//fmt.Printf("pod %s, %s\n", pod.Meta.Name, pod.Meta.Namespace)
-
-		if searchRegexp.MatchString(pod.Meta.Name) {
+		if strings.Contains(pod.Meta.Name, search) {
 			matchingPods = append(matchingPods, pod)
 		}
 	}
